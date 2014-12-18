d2e67e37ac3fa69045f3196baf7fedd4aa092273
sairamd <sairamd@savitr93>
2014-12-18 17:45:14 +0530

    Add Poc Model

---
+++ poc-model/src/main/java/com/pss/poc/ws/model/ClientInfoModel.java
@@ -0,0 +1,44 @@
+package com.pss.poc.ws.model;
+
+public class ClientInfoModel {
+
+	private String clientId;
+	
+	private String clientScrt;
+	
+	private Integer clientStatuscode;
+	
+	private String clientStatusInfo;
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getClientScrt() {
+		return clientScrt;
+	}
+
+	public void setClientScrt(String clientScrt) {
+		this.clientScrt = clientScrt;
+	}
+
+	public Integer getClientStatuscode() {
+		return clientStatuscode;
+	}
+
+	public void setClientStatuscode(Integer clientStatuscode) {
+		this.clientStatuscode = clientStatuscode;
+	}
+
+	public String getClientStatusInfo() {
+		return clientStatusInfo;
+	}
+
+	public void setClientStatusInfo(String clientStatusInfo) {
+		this.clientStatusInfo = clientStatusInfo;
+	}
+}
